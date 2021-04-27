8710f4171a604cf5e8478b16ab77355907aeff81
Lucas Cruz <lucascs20182@gmail.com>
2021-04-27 11:27:42 -0300

    exemplifique sintaxe do casting

---
+++ Aula2/src/exercicios/Casting.java
@@ -0,0 +1,13 @@
+package exercicios;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		double valor1 = 14.52;
+		double valor2 = (double) (12.1f);	
+		int soma = (int) (valor1 + valor2);
+		
+		System.out.println(soma / 2);
+	}
+
+}
