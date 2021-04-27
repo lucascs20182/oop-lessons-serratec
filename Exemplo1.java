aef636ba1b7351ef9d8ffcb123eb5084a8ba2bc8
Lucas Cruz <lucascs20182@gmail.com>
2021-04-27 07:40:54 -0300

    suba os primeiros códigos para o repo

---
+++ Exemplo1.java
@@ -0,0 +1,9 @@
+class Exemplo1 {
+	public static void main(String args[]) {
+		// para compilar abra a pasta deste arquivo no terminal
+		// e digite "javac Exemplo1.java"
+		// um arquivo Exemplo1.class será gerado
+		// então digite "java Exemplo1" para executar
+		System.out.print("Hello world!");
+	}
+}
---
+++ Exemplo2.java
@@ -0,0 +1,21 @@
+class Exemplo2 {
+	public static void main(String args[]) {
+		System.out.print("Lucas\n");
+		System.out.print("Cruz");
+
+		// tabulação
+		System.out.print("\n\tLucas Cruz");
+
+		// backspace: se \b\b apagará o "uz" do print acima
+		System.out.println("\b\bLucas Cruz");
+
+		// printa uma contra-barra (\)
+		System.out.println("\\Lucas Cruz");
+
+		// printa uma aspas simples (')
+		System.out.println("\'Lucas Cruz");
+
+		// printa uma aspas dupla (")
+		System.out.println("\"Lucas Cruz");
+	}
+}
